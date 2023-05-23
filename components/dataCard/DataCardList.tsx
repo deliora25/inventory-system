8b3f231b474aee7ee55e641d07ae488f109fc6f6
Gabriel <gabrielgnzls94@gmail.com>
2023-05-23 08:24:33 -0700

    DataCardList Component: added snackbar on data fetch success and data fetch error

--- components/dataCard/DataCardList.tsx
+++ components/dataCard/DataCardList.tsx
@@ -1,15 +1,45 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DataCard from "./DataCard";
 import { DataAmount } from "@/types";
 import axios from "axios";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function DataCardList() {
   const [data, setData] = useState<DataAmount[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   useEffect(() => {
     const getDataAmount = async () => {
-      const response = await axios.get("http://localhost:4000/dataAmount");
-      setData(response.data);
+      try {
+        const response = await axios.get("http://localhost:4000/dataAmount");
+        if (response && response.data) {
+          setData(response.data);
+          setOpen(true);
+        } else {
+          setOpen(true);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     };
 
     getDataAmount();
@@ -25,6 +55,18 @@ function DataCardList() {
           </div>
         );
       })}
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert
+          onClose={handleClose}
+          severity={data && data.length > 0 ? "success" : "error"}
+          sx={{ width: "100%" }}
+        >
+          {data && data.length > 0
+            ? "Data fetched successfully!"
+            : "Error! No data!"}
+        </Alert>
+      </Snackbar>
+      ;
     </div>
   );
 }
