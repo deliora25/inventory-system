f1711a9faf790ec103a313f470a7291aee22c098
Gabriel <gabrielgnzls94@gmail.com>
2023-05-25 10:00:29 -0700

    SalesDropdown components refactored.

--- components/orders/dropdowns/sales/SalesDropdown.tsx
+++ components/orders/dropdowns/sales/SalesDropdown.tsx
@@ -1,7 +1,7 @@
 import { Menu } from "@headlessui/react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 import { SalesDataType } from "@/types";
-import SalesDropdownItem from "./SalesDropdownItem";
+import SalesDropdownItem from "./SalesDropdownItems";
 
 type Props = {
   salesData: SalesDataType[];
