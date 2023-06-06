4713d126385ab7502b3ac653e49ba20a2591a432
Gabriel <gabrielgnzls94@gmail.com>
2023-06-06 08:08:37 -0700

    Creat Modal common component. Refactor NewProductModal and EditProductModal components.

--- components/product/EditProductForm.tsx
+++ components/product/EditProductForm.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useRouter } from 'next/router';
 import React, { useRef, useState } from 'react';
 import Button from '../common/Button';
+import Input from '../common/Input';
 
 type Props = {
   onClose: () => void;
@@ -52,37 +53,27 @@ function EditProductForm({ onClose, product }: Props) {
 
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <div className="space-y-12">
+      <div className="space-y-12 ">
         <div className="border-b border-gray-900/10 pb-12">
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ">
-            <div className="sm:col-span-4 ">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Product Name
-              </label>
-              <div className="mt-2">
-                <input
-                  value={editProduct.productName}
-                  onChange={handleChange}
-                  name="productName"
-                  type="text"
-                  placeholder="Product Name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-              <div>
-                <label>Category</label>
-                <input
-                  value={editProduct.categoryName}
-                  onChange={handleChange}
-                  name="categoryName"
-                  type="text"
-                  placeholder="Category Name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="sm:col-span-4  ">
+              <Input
+                value={editProduct.productName}
+                name="productName"
+                onChange={handleChange}
+                type="text"
+                placeholder="Product Name"
+                title="Product Name"
+              />
+
+              <Input
+                value={editProduct.categoryName}
+                onChange={handleChange}
+                name="categoryName"
+                type="text"
+                placeholder="Category Name"
+                title="Category Name"
+              />
             </div>
           </div>
         </div>
@@ -93,12 +84,16 @@ function EditProductForm({ onClose, product }: Props) {
           onClick={onClose}
           ref={cancelButtonRef}
           type="button"
-          className="bg-slate-100 text-black hover:bg-slate-200"
+          variant="cancelButton"
         >
           Cancel
         </Button>
 
-        <Button onClick={() => handleUpdate(product.id)} type="submit">
+        <Button
+          onClick={() => handleUpdate(product.id)}
+          type="submit"
+          variant="submitButton"
+        >
           Save
         </Button>
       </div>
